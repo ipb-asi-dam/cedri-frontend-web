f702fd5a2d9a74224aa8b8441b6e51300fccd605
Kaio <duartedossantos@gmail.com>
2019-04-26 23:03:51 +0100

    remove some unused settings

--- src/components/text-editor/index.js
+++ src/components/text-editor/index.js
@@ -1,34 +1,44 @@
-import React, { useCallback, useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import ReactQuill from 'react-quill'
+import InputLabel from '@material-ui/core/InputLabel'
 
 import 'react-quill/dist/quill.snow.css'
 
-function TextEditor ({ content }) {
-  const [text, handleChange] = useState(content)
-  const handleEditorChange = useCallback((value) => handleChange(value))
+function TextEditor ({ isSubmiting, ...props }) {
+  const { name, label } = props
 
   return (
-    <ReactQuill
-      modules={{
-        toolbar: [
-          ['bold', 'italic'],
-          ['link', 'image']
-        ]
-      }}
-      placeholder='Type here your text'
-      onChange={handleEditorChange}
-      value={text}
-    />
+    <>
+      <InputLabel htmlFor={name}>
+        {label}
+      </InputLabel>
+      <ReactQuill
+        modules={{
+          toolbar: [
+            ['bold', 'italic'],
+            ['link', 'image']
+          ]
+        }}
+        placeholder='Type here your text'
+        readOnly={isSubmiting}
+        style={{ marginTop: 10 }}
+        {...props}
+      />
+      <input hidden {...props} />
+    </>
   )
 }
 
 TextEditor.defaultProps = {
-  content: ''
+  isSubmiting: false,
+  value: ''
 }
 
 TextEditor.propTypes = {
-  content: PropTypes.string
+  isSubmiting: PropTypes.bool,
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired
 }
 
 export default TextEditor
