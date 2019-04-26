fd19e4328de77bab87d10fbe40a57d50aebebddc
Kaio <duartedossantos@gmail.com>
2019-04-26 22:57:17 +0100

    update navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,22 +1,24 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import t from 'prop-types'
+import classnames from 'classnames'
 import AppBar from '@material-ui/core/AppBar'
-import Avatar from '@material-ui/core/Avatar'
 import Grid from '@material-ui/core/Grid'
 import Hidden from '@material-ui/core/Hidden'
 import IconButton from '@material-ui/core/IconButton'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import AccountCircle from '@material-ui/icons/AccountCircle'
 import MenuIcon from '@material-ui/icons/Menu'
-import NotificationsIcon from '@material-ui/icons/Notifications'
 import Toolbar from '@material-ui/core/Toolbar'
-import Tooltip from '@material-ui/core/Tooltip'
 import Typography from '@material-ui/core/Typography'
 import { withStyles } from '@material-ui/core/styles'
 
 const drawerWidth = 240
-const lightColor = 'rgba(255, 255, 255, 0.7)'
 
 const styles = theme => ({
   appBar: {
+    height: 65,
+    justifyContent: 'center',
     transition: theme.transitions.create(['margin', 'width'], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen
@@ -25,6 +27,7 @@ const styles = theme => ({
   appBarShift: {
     width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: drawerWidth,
+    paddingLeft: 24,
     transition: theme.transitions.create(['margin', 'width'], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen
@@ -32,69 +35,76 @@ const styles = theme => ({
   },
   menuButton: {
     marginLeft: -theme.spacing.unit
-  },
-  iconButtonAvatar: {
-    padding: 4
-  },
-  link: {
-    textDecoration: 'none',
-    color: lightColor,
-    '&:hover': {
-      color: theme.palette.common.white
-    }
-  },
-  button: {
-    borderColor: lightColor
   }
 })
 
-const Header = ({ classes, onDrawerToggle, smDown = false }) => (
-  <AppBar
-    color='primary'
-    component='div'
-    elevation={0}
-    position={smDown ? 'fixed' : 'static'}
-  >
-    <Toolbar>
-      <Grid container alignItems='center' spacing={8}>
-        <Hidden mdUp>
-          <Grid item>
-            <IconButton
-              color='inherit'
-              aria-label='Open drawer'
-              onClick={onDrawerToggle}
-              className={classes.menuButton}
-            >
-              <MenuIcon />
-            </IconButton>
-          </Grid>
-        </Hidden>
-        <Grid item xs>
-          <Typography color='inherit' variant='h6'>
-            Authentication
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Tooltip title='Alerts • No alters'>
-            <IconButton color='inherit'>
-              <NotificationsIcon />
-            </IconButton>
-          </Tooltip>
-        </Grid>
-        <Grid item>
-          <IconButton color='inherit' className={classes.iconButtonAvatar}>
-            <Avatar className={classes.avatar} src='/static/images/avatar/1.jpg' />
-          </IconButton>
-        </Grid>
-      </Grid>
-    </Toolbar>
-  </AppBar>
-)
+const Navbar = ({ classes, onDrawerToggle, smDown, title }) => {
+  const [anchorElement, setAnchorElement] = useState(null)
 
-Header.propTypes = {
-  classes: t.object.isRequired,
-  onDrawerToggle: t.func.isRequired,
-  smDown: t.bool
+  const handleClose = useCallback(() => setAnchorElement(null))
+  const handleOpenMenu = useCallback((e) => setAnchorElement(e.target))
+  const logout = useCallback(() => {
+    window.alert('logout')
+    handleClose() // remove when private routes are implemented
+  })
+
+  return (
+    <AppBar
+      className={classnames(
+        classes.appBar,
+        !smDown && classes.appBarShift
+      )}
+      color='primary'
+      component='header'
+      elevation={0}
+      position='fixed'
+    >
+      <Toolbar>
+        <Grid container alignItems='center' spacing={8}>
+          <Hidden mdUp>
+            <Grid item>
+              <IconButton
+                color='inherit'
+                aria-label='Open drawer'
+                onClick={onDrawerToggle}
+                className={classes.menuButton}
+              >
+                <MenuIcon />
+              </IconButton>
+            </Grid>
+          </Hidden>
+          <Grid item xs>
+            <Typography color='inherit' variant='h6'>
+              {title}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <IconButton color='inherit' onClick={handleOpenMenu}>
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              open={Boolean(anchorElement)}
+              onClose={handleClose}
+              anchorEl={anchorElement}
+            >
+              <MenuItem onClick={logout}>Logout</MenuItem>
+            </Menu>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  )
 }
 
-export default withStyles(styles)(Header)
+Navbar.defaultProps = {
+  smDown: false
+}
+
+Navbar.propTypes = {
+  classes: t.object.isRequired,
+  onDrawerToggle: t.func.isRequired,
+  smDown: t.bool,
+  title: t.string.isRequired
+}
+
+export default withStyles(styles)(Navbar)
