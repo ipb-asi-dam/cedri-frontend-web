b3132d214d3311996d6904e57107cffc89b31584
Kaio <duartedossantos@gmail.com>
2019-06-22 23:48:33 +0100

    update account page

--- src/pages/account/index.js
+++ src/pages/account/index.js
@@ -1,33 +1,64 @@
-import React, { useContext, useEffect, useState } from 'react'
-import axios from 'config/axios'
+import React, { useContext, useRef, useState } from 'react'
 import PropTypes from 'prop-types'
+import axios from 'config/axios'
 
 import Form from 'react-vanilla-form'
+import Avatar from '@material-ui/core/Avatar'
 import Button from '@material-ui/core/Button'
 import Divider from '@material-ui/core/Divider'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
-import withStyles from '@material-ui/core/styles/withStyles'
 
 import TextField from 'components/text-field'
+import TextFieldVisibility from 'components/text-field-visibility'
+
+// contexts
 import { AuthContext } from 'contexts/auth'
+import { SnackbarContext } from 'contexts/snackbar'
 
-import styles from './styles'
+import useStyles from './styles'
+import { strongPassword, required, passwordLength } from 'utils/validations'
 
-function Account ({ classes }) {
-  const [formData, setFormData] = useState({})
-  const { userInfo: { user } } = useContext(AuthContext)
+function Account ({ history }) {
+  const classes = useStyles()
 
-  useEffect(() => {
-    (async () => {
-      const response = await axios.get(`/private/users/${user.id}`)
-      setFormData({
-        ...response.data.data,
-        ...response.data.data.user
+  const { showNotification } = useContext(SnackbarContext)
+  const { userInfo: { user }, updateUserInfo } = useContext(AuthContext)
+
+  const [isValid, setValid] = useState(false)
+  const [isSubmitting, setSubmitting] = useState(false)
+  const [formData] = useState({ ...user })
+  const imageRef = useRef()
+  const [image, setImage] = useState()
+
+  function handleOnChangeImage (e) {
+    setImage(e.target.files[0])
+  }
+
+  async function onSubmit (data, errors) {
+    const content = new FormData()
+
+    content.append('image', image)
+
+    Object
+      .entries(data)
+      .forEach(([key, val]) => {
+        content.append(key, val)
       })
-    })()
-  }, [user.id])
+
+    try {
+      setSubmitting(true)
+      await axios.put(`/private/users/${user.id}`, content)
+      await updateUserInfo(user.id)
+      showNotification('User successfully updated')
+      setSubmitting(false)
+    } catch {
+      showNotification('Error during user update')
+    } finally {
+      history.push('/dashboard')
+    }
+  }
 
   return (
     <div className={classes.root}>
@@ -42,51 +73,77 @@ function Account ({ classes }) {
               Profile
             </Typography>
           </div>
-          <Form data={formData}>
+          <Form
+            data={formData}
+            onChange={(_, errors) => setValid(Object.keys(errors).length === 0)}
+            onSubmit={onSubmit}
+            validation={{
+              password: [required, passwordLength, strongPassword]
+            }}
+          >
             <Grid container spacing={3}>
               <Grid container justify='center' style={{ paddingBottom: 20 }}>
-                <img
+                <Avatar
                   alt=''
-                  style={{ borderRadius: '50%', cursor: 'pointer' }}
-                  src='//via.placeholder.com/100'
+                  className={classes.avatar}
+                  src={user.imageURL}
+                />
+                <input
+                  className={classes.avatarBtn}
+                  onChange={handleOnChangeImage}
+                  ref={imageRef}
+                  style={{ width: 100, height: 100 }}
+                  type='file'
                 />
               </Grid>
-              <Grid item xs={6}>
+              <Grid item xs={12} md={6}>
                 <TextField
+                  disabled={isSubmitting}
                   label='Name'
                   name='name'
                 />
               </Grid>
-              <Grid item xs={6}>
+              <Grid item xs={12} md={6}>
                 <TextField
+                  disabled={isSubmitting}
                   label='Email'
                   name='email'
                 />
               </Grid>
-              <Grid item xs={6}>
+              <Grid item xs={12} md={6}>
                 <TextField
+                  disabled={isSubmitting}
                   label='Occupation'
                   name='occupation'
                 />
               </Grid>
-              <Grid item xs={6}>
+              <Grid item xs={12} md={6}>
                 <TextField
+                  disabled={isSubmitting}
                   label='Bio'
                   name='bio'
                 />
               </Grid>
+              <Grid item xs={12} md={6}>
+                <TextFieldVisibility
+                  disabled={isSubmitting}
+                  label='Password'
+                  name='password'
+                  type='password'
+                />
+              </Grid>
               <Divider />
-              <div style={{
-                display: 'flex',
-                justifyContent: 'flex-end',
-                paddingRight: 10,
-                paddingTop: 10,
-                width: '100%'
-              }}>
-                <Button color='primary' type='submit'>
+              <Grid className={classes.formBtn} item xs={12}>
+                <Button
+                  color='primary'
+                  disabled={!isValid || isSubmitting}
+                  fullWidth
+                  type='submit'
+                  variant='contained'
+                >
                   Update
                 </Button>
-              </div>
+              </Grid>
             </Grid>
           </Form>
         </Paper>
@@ -96,7 +153,7 @@ function Account ({ classes }) {
 }
 
 Account.propTypes = {
-  classes: PropTypes.object.isRequired
+  history: PropTypes.object.isRequired
 }
 
-export default withStyles(styles)(Account)
+export default Account
--- src/pages/account/styles.js
+++ src/pages/account/styles.js
@@ -1,5 +1,29 @@
-export default (theme) => ({
+import makeStyles from '@material-ui/core/styles/makeStyles'
+
+const styles = (theme) => ({
   root: {
 
+  },
+  avatar: {
+    borderRadius: '50%',
+    height: '100px',
+    width: '100px'
+  },
+  avatarBtn: {
+    opacity: 0,
+    cursor: 'pointer',
+    position: 'absolute',
+    '&:hover': {
+      border: '1px solid grey',
+      zIndex: 100
+    }
+  },
+  formBtn: {
+    display: 'flex',
+    justifyContent: 'center',
+    padding: `10px ${theme.spacing(3)}px`,
+    width: '100%'
   }
 })
+
+export default makeStyles(styles)
