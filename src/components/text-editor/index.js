ed3c70372f1e75c119fc43f04c6bbc6767b3030c
Kaio <duartedossantos@gmail.com>
2019-06-23 23:45:55 +0100

    update text editor (remove image)

--- src/components/text-editor/index.js
+++ src/components/text-editor/index.js
@@ -5,7 +5,7 @@ import InputLabel from '@material-ui/core/InputLabel'
 
 import 'react-quill/dist/quill.snow.css'
 
-function TextEditor ({ disabled, ...props }) {
+function TextEditor ({ disabled, value, ...props }) {
   const { name, label } = props
 
   return (
@@ -14,10 +14,11 @@ function TextEditor ({ disabled, ...props }) {
         {label}
       </InputLabel>
       <ReactQuill
+        defaultValue={value}
         modules={{
           toolbar: [
             ['bold', 'italic'],
-            ['link', 'image']
+            ['link']
           ]
         }}
         placeholder='Type here your text'
@@ -25,7 +26,6 @@ function TextEditor ({ disabled, ...props }) {
         style={{ marginTop: 10 }}
         {...props}
       />
-      <input hidden {...props} />
     </>
   )
 }
@@ -38,7 +38,9 @@ TextEditor.defaultProps = {
 TextEditor.propTypes = {
   disabled: PropTypes.bool,
   name: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func,
+  value: PropTypes.any
 }
 
 export default TextEditor
